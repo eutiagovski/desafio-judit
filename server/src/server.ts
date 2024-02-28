95f726f0e1a6b91a58878f6649ca8cfc7d840e07
Tiago Machado <tiagomachadorj@hotmail.com>
2024-02-28 11:40:28 -0300

    mongoose initial config

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,22 @@
-import express from 'express';
-import { json } from 'body-parser';
+import express from "express";
+import { json } from "body-parser";
+import mongoose from "mongoose";
 
-import { todoRouter } from './routes/todo.route';
+// importing routes
+import { todoRouter } from "./routes/todo.route";
 
+// initialize app
 const app = express();
 
 app.use(json());
-app.use(todoRouter)
+app.use(todoRouter);
+
+// connect to database
+mongoose.connect(`mongodb+srv://judit-db-user:Eu0NDDxrTE4wRbrP@judit-challange.kt0ngvs.mongodb.net/?retryWrites=true&w=majority&appName=judit-challange`)
+.then(() => {
+    console.log('Connected to database')
+});
 
 app.listen(3000, () => {
-    console.log('Server has initiated on port 3000')
-})
+  console.log("Server has initiated on port 3000");
+});
