e273f625e3f88a2f85b34f5434499f8c86baacc5
Tiago Machado <tiagomachadorj@hotmail.com>
2024-03-01 11:40:33 -0300

    created a route to see the logs

--- server/src/routes/routes.ts
+++ server/src/routes/routes.ts
@@ -2,9 +2,9 @@ import express from "express";
 
 const router = express.Router();
 
-router.post("/capture", require("./capture"));
-router.post("/capture/move", require("./moveCapture"));
-router.get("/capture/list", require("./listCaptures"));
+router.post("/capture", require("./capture/capture.route"));
+router.post("/capture/move", require("./capture/move.route"));
+router.get("/capture/list", require("./capture/list.route"));
 
 router.get("/logs", require("./log/log.route"));
 
